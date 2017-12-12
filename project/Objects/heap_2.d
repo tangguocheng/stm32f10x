d0a160206f5b98a5c0edc0e30fc8627755978ecb
tgc <tgcdz@outlook.com>
2017-12-12 22:40:48 +0800

    smart home base project

---
+++ project/Objects/heap_2.d
@@ -0,0 +1,13 @@
+.\objects\heap_2.o: ..\lib\FreeRTOS\Source\portable\MemMang\heap_2.c
+.\objects\heap_2.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+.\objects\heap_2.o: ..\lib\FreeRTOS\Source\include\FreeRTOS.h
+.\objects\heap_2.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+.\objects\heap_2.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\heap_2.o: ..\..\STM32F103\FreeRTOSConfig.h
+.\objects\heap_2.o: ..\lib\FreeRTOS\Source\include\projdefs.h
+.\objects\heap_2.o: ..\lib\FreeRTOS\Source\include\portable.h
+.\objects\heap_2.o: ..\lib\FreeRTOS\Source\include\deprecated_definitions.h
+.\objects\heap_2.o: ..\lib\FreeRTOS\Source\portable\RVDS\ARM_CM3\portmacro.h
+.\objects\heap_2.o: ..\lib\FreeRTOS\Source\include\mpu_wrappers.h
+.\objects\heap_2.o: ..\lib\FreeRTOS\Source\include\task.h
+.\objects\heap_2.o: ..\lib\FreeRTOS\Source\include\list.h
---
+++ project/Objects/queue.d
@@ -0,0 +1,15 @@
+.\objects\queue.o: ..\lib\FreeRTOS\Source\queue.c
+.\objects\queue.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+.\objects\queue.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+.\objects\queue.o: ..\lib\FreeRTOS\Source\include\FreeRTOS.h
+.\objects\queue.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+.\objects\queue.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\queue.o: ..\..\STM32F103\FreeRTOSConfig.h
+.\objects\queue.o: ..\lib\FreeRTOS\Source\include\projdefs.h
+.\objects\queue.o: ..\lib\FreeRTOS\Source\include\portable.h
+.\objects\queue.o: ..\lib\FreeRTOS\Source\include\deprecated_definitions.h
+.\objects\queue.o: ..\lib\FreeRTOS\Source\portable\RVDS\ARM_CM3\portmacro.h
+.\objects\queue.o: ..\lib\FreeRTOS\Source\include\mpu_wrappers.h
+.\objects\queue.o: ..\lib\FreeRTOS\Source\include\task.h
+.\objects\queue.o: ..\lib\FreeRTOS\Source\include\list.h
+.\objects\queue.o: ..\lib\FreeRTOS\Source\include\queue.h
