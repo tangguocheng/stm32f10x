0ce466f48d49c78bdfbafd298397ac0b466f50de
tgcdz <tgcdz@outlook.com>
2017-12-27 19:34:15 +0800

    pc

--- pc_tools/ModbusTCPUpdate/ModbusTCPUpdate/ModbusTCPUpdate/Form1.Designer.cs
+++ pc_tools/ModbusTCPUpdate/ModbusTCPUpdate/ModbusTCPUpdate/Form1.Designer.cs
@@ -29,12 +29,20 @@
                 private void InitializeComponent()
                 {
                         this.btx_open_server = new System.Windows.Forms.Button();
-                        this.textBox1 = new System.Windows.Forms.TextBox();
+                        this.function_area = new System.Windows.Forms.TabControl();
+                        this.tabPage1 = new System.Windows.Forms.TabPage();
+                        this.tab_update = new System.Windows.Forms.TabPage();
+                        this.tab_setting = new System.Windows.Forms.TabPage();
+                        this.txb_info = new System.Windows.Forms.RichTextBox();
+                        this.button1 = new System.Windows.Forms.Button();
+                        this.function_area.SuspendLayout();
+                        this.tabPage1.SuspendLayout();
+                        this.tab_setting.SuspendLayout();
                         this.SuspendLayout();
                         // 
                         // btx_open_server
                         // 
-                        this.btx_open_server.Location = new System.Drawing.Point(94, 152);
+                        this.btx_open_server.Location = new System.Drawing.Point(172, 434);
                         this.btx_open_server.Name = "btx_open_server";
                         this.btx_open_server.Size = new System.Drawing.Size(75, 23);
                         this.btx_open_server.TabIndex = 0;
@@ -42,32 +50,94 @@
                         this.btx_open_server.UseVisualStyleBackColor = true;
                         this.btx_open_server.Click += new System.EventHandler(this.btx_open_server_Click);
                         // 
-                        // textBox1
+                        // function_area
                         // 
-                        this.textBox1.Location = new System.Drawing.Point(12, 12);
-                        this.textBox1.Name = "textBox1";
-                        this.textBox1.Size = new System.Drawing.Size(372, 21);
-                        this.textBox1.TabIndex = 1;
+                        this.function_area.Controls.Add(this.tabPage1);
+                        this.function_area.Controls.Add(this.tab_update);
+                        this.function_area.Controls.Add(this.tab_setting);
+                        this.function_area.Location = new System.Drawing.Point(12, 12);
+                        this.function_area.Multiline = true;
+                        this.function_area.Name = "function_area";
+                        this.function_area.SelectedIndex = 0;
+                        this.function_area.Size = new System.Drawing.Size(504, 505);
+                        this.function_area.TabIndex = 3;
+                        // 
+                        // tabPage1
+                        // 
+                        this.tabPage1.Controls.Add(this.button1);
+                        this.tabPage1.Controls.Add(this.txb_info);
+                        this.tabPage1.Location = new System.Drawing.Point(4, 22);
+                        this.tabPage1.Name = "tabPage1";
+                        this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+                        this.tabPage1.Size = new System.Drawing.Size(496, 479);
+                        this.tabPage1.TabIndex = 0;
+                        this.tabPage1.Text = "Modbus";
+                        this.tabPage1.UseVisualStyleBackColor = true;
+                        // 
+                        // tab_update
+                        // 
+                        this.tab_update.Location = new System.Drawing.Point(4, 22);
+                        this.tab_update.Name = "tab_update";
+                        this.tab_update.Padding = new System.Windows.Forms.Padding(3);
+                        this.tab_update.Size = new System.Drawing.Size(496, 479);
+                        this.tab_update.TabIndex = 1;
+                        this.tab_update.Text = "固件更新";
+                        this.tab_update.UseVisualStyleBackColor = true;
+                        // 
+                        // tab_setting
+                        // 
+                        this.tab_setting.Controls.Add(this.btx_open_server);
+                        this.tab_setting.Location = new System.Drawing.Point(4, 22);
+                        this.tab_setting.Name = "tab_setting";
+                        this.tab_setting.Padding = new System.Windows.Forms.Padding(3);
+                        this.tab_setting.Size = new System.Drawing.Size(496, 479);
+                        this.tab_setting.TabIndex = 2;
+                        this.tab_setting.Text = "设置";
+                        this.tab_setting.UseVisualStyleBackColor = true;
+                        // 
+                        // txb_info
+                        // 
+                        this.txb_info.Location = new System.Drawing.Point(24, 18);
+                        this.txb_info.Name = "txb_info";
+                        this.txb_info.Size = new System.Drawing.Size(427, 204);
+                        this.txb_info.TabIndex = 2;
+                        this.txb_info.Text = "";
+                        // 
+                        // button1
+                        // 
+                        this.button1.Location = new System.Drawing.Point(212, 314);
+                        this.button1.Name = "button1";
+                        this.button1.Size = new System.Drawing.Size(75, 23);
+                        this.button1.TabIndex = 3;
+                        this.button1.Text = "button1";
+                        this.button1.UseVisualStyleBackColor = true;
+                        this.button1.Click += new System.EventHandler(this.button1_Click);
                         // 
                         // Form1
                         // 
                         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
                         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-                        this.ClientSize = new System.Drawing.Size(438, 262);
-                        this.Controls.Add(this.textBox1);
-                        this.Controls.Add(this.btx_open_server);
+                        this.ClientSize = new System.Drawing.Size(961, 512);
+                        this.Controls.Add(this.function_area);
                         this.Name = "Form1";
                         this.Text = "Form1";
                         this.Load += new System.EventHandler(this.Form1_Load);
+                        this.function_area.ResumeLayout(false);
+                        this.tabPage1.ResumeLayout(false);
+                        this.tab_setting.ResumeLayout(false);
                         this.ResumeLayout(false);
-                        this.PerformLayout();
 
                 }
 
                 #endregion
 
                 private System.Windows.Forms.Button btx_open_server;
-                private System.Windows.Forms.TextBox textBox1;
+                private System.Windows.Forms.TabControl function_area;
+                private System.Windows.Forms.TabPage tabPage1;
+                private System.Windows.Forms.TabPage tab_update;
+                private System.Windows.Forms.RichTextBox txb_info;
+                private System.Windows.Forms.TabPage tab_setting;
+                private System.Windows.Forms.Button button1;
         }
 }
 
--- pc_tools/ModbusTCPUpdate/ModbusTCPUpdate/ModbusTCPUpdate/Form1.cs
+++ pc_tools/ModbusTCPUpdate/ModbusTCPUpdate/ModbusTCPUpdate/Form1.cs
@@ -18,20 +18,31 @@ namespace ModbusTCPUpdate
                         InitializeComponent();
                 }
 
+                ~Form1()
+                {
+                        if (isPortOpen)
+                        {
+                                tcpServer.ServerClose();
+                        }
+                }
+
                 private void Form1_Load(object sender, EventArgs e)
                 {
 
                 }
+
                 private ModBusTcpServer tcpServer;
                 private bool isPortOpen = false;
                 private void btx_open_server_Click(object sender, EventArgs e)
                 {
                         if (!isPortOpen)
                         {
-                                tcpServer = new ModBusTcpServer();
+                                tcpServer = new ModBusTcpServer();                               
                                 tcpServer.LogNet = new HslCommunication.LogNet.LogNetSingle(Application.StartupPath + @"\Logs\log.txt");
                                 tcpServer.OnDataReceived += TcpServer_OnDataReceived;
                                 tcpServer.ServerStart(502);
+                                isPortOpen = true;
+                                btx_open_server.Text = "close";
                         }
 
                 }
@@ -41,11 +52,29 @@ namespace ModbusTCPUpdate
                         BeginInvoke(new Action<byte[]>(ShowModbusData), object1);
                 }
 
-
                 private void ShowModbusData(byte[] modbus)
                 {
-                        textBox1.AppendText(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + " :" +
-                            HslCommunication.BasicFramework.SoftBasic.ByteToHexString(modbus) + Environment.NewLine);
+                        string transaction_id = BitConverter.ToString(modbus,0,2);
+                        string protocol_id = BitConverter.ToString(modbus, 2, 2); ;
+                        string length = BitConverter.ToString(modbus, 4, 1); ;
+                        string unit_id = BitConverter.ToString(modbus, 5, 1); ;
+                        string Fcode = BitConverter.ToString(modbus,6, 1); ;
+                        string data = BitConverter.ToString(modbus, 7); ;
+
+                        txb_info.AppendText(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + " :" + Environment.NewLine+
+                            "transaction_id: " + transaction_id + Environment.NewLine +
+                            "protocol_id: " + protocol_id + Environment.NewLine +
+                            "length: " + length + Environment.NewLine +
+                            "unit_id: " + unit_id + Environment.NewLine +
+                            "Fcode: " + Fcode + Environment.NewLine +
+                            "data: " + data + Environment.NewLine);
+                }
+
+                private void button1_Click(object sender, EventArgs e)
+                {
+                        tcpServer.WriteRegister(0, 12);
+                        byte[] test = tcpServer.ReadRegister(0, 1);
+                        txb_info.AppendText(BitConverter.ToString(test));
                 }
         }
 }
